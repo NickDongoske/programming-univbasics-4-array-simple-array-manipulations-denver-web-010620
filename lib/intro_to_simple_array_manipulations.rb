c4581d68b97ad83541af94244d09566f815bc5a4
Nicholas Dongoske <nickdongoske@gmail.com>
2019-12-26 16:18:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push(array, string)
+  using_push = array
+  array.push("violet")
+end
+
+def using_unshift(array, string)
+  using_unshift = array
+  array.unshift("Staten Island")
+end
+
+def using_pop(array)
+  using_pop = array
+  array.pop()
+end
+
+def pop_with_args(array)
+  pop_with_args = array
+  array.pop()
+  array.pop()
+end
